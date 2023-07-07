97348db77091b490d9904ea26f89d2fcddb90082
Saint Laurent <stlaurentcodes@gmail.com>
2023-07-07 11:57:42 +0100

    first push

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,19 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+	return (
+		<Html lang='en'>
+			<Head>
+				<title>Infinix Slot</title>
+				<link
+					rel='stylesheet'
+					href='https://unpkg.com/flowbite@1.3.3/dist/flowbite.min.css'
+				/>
+			</Head>
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
 }
