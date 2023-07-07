b1b0a94055385228607b3dc3437cb3a20e2463d5
Saint Laurent <stlaurentcodes@gmail.com>
2023-07-07 13:03:38 +0100

    fix: add more regions

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -8,36 +8,80 @@ const Home = () => {
 
 	const regions = [
 		{
-			name: 'Abuja',
-			initial: 'Ab',
-		},
-		{
 			name: 'Lagos',
 			initial: 'La',
+		},
+		{
+			name: 'Port Harcourt',
+			initial: 'Ph',
 		},
 		{
 			name: 'Ibadan',
 			initial: 'Ib',
 		},
 		{
+			name: 'Kaduna',
+			initial: 'Kd',
+		},
+		{
+			name: 'Kano',
+			initial: 'Kn',
+		},
+		{
 			name: 'Onitsha',
 			initial: 'On',
+		},
+		{
+			name: 'Abuja',
+			initial: 'Ab',
+		},
+		{
+			name: 'Royaling',
+			initial: 'Ro',
+		},
+		{
+			name: 'Mchris',
+			initial: 'Mc',
+		},
+		{
+			name: 'Ponitek',
+			initial: 'Po',
+		},
+		{
+			name: 'Top Success',
+			initial: 'Ts',
+		},
+		{
+			name: 'Finet',
+			initial: 'Fi',
+		},
+		{
+			name: '3CHub',
+			initial: '3c',
+		},
+		{
+			name: 'Fine Brothers',
+			initial: 'Fb',
+		},
+		{
+			name: 'Slot',
+			initial: 'Sl',
 		},
 	];
 
 	return (
-		<div className='h-screen w-screen bg-green-700 p-6'>
+		<div className='h-full md:h-screen w-screen bg-green-700 p-6'>
 			<h3 className='text-3xl uppercase text-white mb-12'>Regions</h3>
 
 			{regions.length > 0 && (
-				<div className='flex justify-center md:justify-start md:px-12 gap-4 flex-wrap'>
+				<div className='grid grid-cols-2 md:grid-cols-6 gap-4'>
 					{regions.map((region) => (
 						<div
 							key={region.initial}
-							className='bg-green-300 p-3 w-24 flex flex-col gap-3 justify-center items-center rounded-md cursor-pointer'
+							className='bg-green-300 p-3 basis-1/2 md:basis-1/4 flex flex-col gap-3 justify-center items-center rounded-md cursor-pointer'
 							onClick={() => router.push(`/${region.name.toLowerCase()}`)}
 						>
-							<p className='p-3 bg-black text-white rounded uppercase w-[65%] text-center'>
+							<p className='p-3 bg-black text-white rounded uppercase w-[65%] md:w-[50%] text-center'>
 								{region.initial}
 							</p>
 							<p>{region.name}</p>
